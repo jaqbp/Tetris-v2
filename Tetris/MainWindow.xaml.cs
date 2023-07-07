bd8274850f9ad76650dca2597850cff4624a741e
Jakub Pietrasik <jakubmkjp@gmail.com>
2023-07-07 19:20:04 +0200

    first functionalities

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,12 +21,82 @@ namespace Tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ImageSource[] tileImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/Tiles/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Tiles/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Tiles/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Tiles/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Tiles/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Tiles/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Tiles/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Tiles/TileRed.png", UriKind.Relative))
+        };
+
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/Blocks/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Blocks/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Blocks/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Blocks/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Blocks/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Blocks/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Blocks/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Blocks/Block-Z.png", UriKind.Relative)),
+        };
+
+        private readonly Image[,] imageControls;
+
+        private GameState gameState = new GameState();
+
         public MainWindow()
         {
             InitializeComponent();
+            imageControls = SetupGameCanvas(gameState.GameGrid);
+        }
+
+        private Image[,] SetupGameCanvas(GameGrid grid)
+        {
+            Image[,] imageControls = new Image[grid.Rows, grid.Columns];
+            int cellSize = 25;
+            for (int r=0; r < grid.Rows; r++)
+            {
+                for (int c=0; c<grid.Columns; c++)
+                {
+                    Image imageControl = new Image()
+                    {
+                        Width = cellSize,
+                        Height = cellSize
+                    };
+
+                    Canvas.SetTop(imageControl, (r - 2) * cellSize);
+                    Canvas.SetLeft(imageControl, c * cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+
+            return imageControls;
+        }
+
+        private void DrawGrid(GameGrid grid)
+        {
+            for (int r = 0; r < grid.Rows; r++)
+            {
+                for (int c = 0; c < grid.Columns; c++)
+                {
+                    int id = grid[r, c];
+                    imageControls[r, c].Source = tileImages[id];
+                }
+            }
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
